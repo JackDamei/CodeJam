ad254293ccefc37d53ea6582f71312c97352649a
JackDamei <jackdamei.bj@gmail.com>
2020-05-01 21:36:51 +0200

    2020/05/02 before cj1c

---
+++ src/y2020/r1C/MainA.java
@@ -0,0 +1,32 @@
+package y2020.r1C;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class MainA {
+
+	public static void main (String[] args) {
+
+		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in))); 
+		PrintStream out = System.out;
+		int T = in.nextInt();
+			
+		for (int t=0; t<T; t++) {
+			
+			int N = in.nextInt();
+			String s = in.next();
+			int[] tab = new int[N];
+			for (int i=0; i<N; i++) {
+				tab[i] = in.nextInt();
+			}
+			
+			String res = "";
+			out.println(String.format("Case #%d: %s", t+1, res));
+		}
+		
+		in.close();
+	}
+
+}
